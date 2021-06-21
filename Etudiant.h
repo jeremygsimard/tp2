930b17448b6ce0943099990399d23c906206ca09
jeremygsimard <83423948+jeremygsimard@users.noreply.github.com>
2021-06-21 10:34:48 -0400

    Update Etudiant.h

--- Etudiant.h
+++ Etudiant.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef __ETUDIANT_H__
+#define __ETUDIANT_H__
 #include <vector>
 #include <iostream>
 
@@ -8,15 +9,18 @@ private:
     std::vector<int> notes;
     std::string identifiant;
     long matricule;
-  
+
 public:
     Etudiant();
     Etudiant(std::string un_identifiant, long un_matricule);
 
-
     float calculerMoyenne();
     void ajouterNote(int une_note);
-    long getMatricule();
+    long getMatricule() const;
     bool operator<(const Etudiant& etudiant_a_droite) const;
 
 };
+#endif
+
+
+
